aca330540253ee4fc0448f803cd4e2046fca0183
natali <v-natalii@mail.ru>
2022-10-16 19:18:51 +0300

    add Task 02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход
+// принимает два числа и выдаёт, какое число большее, а
+// какое меньшее.
+
+Console.WriteLine("Введите число a ");
+int numa = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b ");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+if (numb > numa) Console.Write($"Число {numb} больше числа {numa}");
+else if (numb < numa) Console.Write($"Число {numa} больше числа {numb}");
+else Console.Write($"Число {numa} равно числу {numb}");
+
