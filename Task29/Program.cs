b33fa296c95560447d5f2c1751c9fccc11c01606
natali <v-natalii@mail.ru>
2022-11-20 21:57:46 +0300

    add task29

---
+++ Task29/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+int[] NewArray(int len)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = rnd.Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(",");
+    }
+    Console.Write("]");
+}
+
+Console.Write("Введите количество элементов массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+if (length > 0)
+{
+    int[] newarray = NewArray(length);
+    PrintArray(newarray);
+}
+else Console.WriteLine("Введено не верное число элементов массива");
