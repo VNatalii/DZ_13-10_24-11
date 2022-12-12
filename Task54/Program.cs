2fc16a2718016f6cb4600011e576f76e839b65f2
natali <v-natalii@mail.ru>
2022-12-12 19:43:12 +0300

    add task54

---
+++ Task54/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+int[,] CreateMatrixRndDouble(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.Write("");
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write(" | ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}  ");
+            else Console.Write($"{matrix[i, j],5} ");
+
+        }
+        Console.WriteLine(" | ");
+    }
+    Console.Write("");
+}
+
+
+int[,] SortMatrixRowDesc(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int count = 0; count < matrix.GetLength(1); count++)
+            for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+                if (matrix[i, j] < matrix[i, j + 1])
+                {
+                    int temp = matrix[i, j + 1];
+                    matrix[i, j + 1] = matrix[i, j];
+                    matrix[i, j] = temp;
+                }
+
+    }
+    return matrix;
+}
+
+
+Console.Write("Введите количество строк матрицы: ");
+int r = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов матрицы: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минималное значение элемента массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное значение элемента массива: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+int[,] matx = CreateMatrixRndDouble(r, c, a, b);
+PrintMatrix(matx);
+Console.WriteLine("");
+int[,] sortmatx = SortMatrixRowDesc(matx);
+PrintMatrix(sortmatx);
+
